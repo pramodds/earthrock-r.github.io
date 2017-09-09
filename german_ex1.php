aa1a751e2f1ccbb68b8a7bed0e416ad1cb6bd79a
Ranjith376 <31335667+Ranjith376@users.noreply.github.com>
2017-09-09 23:24:51 +0530

    Add files via upload

---
+++ german_ex1.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <title>German | Pronouns</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  </head>
+  <body font: sans-serif>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <div id="header">
+      <div class="container">
+        <div class="header-title">
+          <a href="earthrock-r.github.io/index.html"><img src="logo.png"></a>
+    </div>
+      <div class="header-list">
+        <ul>
+          <li><a href="earthrock-r.github.io/about.html"><h3>ABOUT</h3></a></li>
+          <li><a href="earthrock-r.github.io/contact.html"><h3>CONTACT</h3></a></li>
+          <li><a href="earthrock-r.github.io/courses.html"><h3>COURSES</h3></a></li>
+        </ul>
+      </div>
+    </div class="container">
+    </div>
+    <div id="content">
+      <div class="container">
+        <h1>Answers for your exercise:</h1>
+        <ol>
+          <li><h4>I - Ich. Your answer is <?php if ($_GET["ex1_1"] = "Ich") { echo "correct"; } else { echo "wrong"; } ?> . </h4></li>
+          <li><h4>She - sie. Your answer is <?php if ($_GET["ex1_2"] = "sie") { echo "correct"; } else { echo "wrong"; } ?> . </h4></li>
+          <li><h4>We - wir. Your answer is <?php if ($_GET["ex1_3"] = "wir") { echo "correct"; } else { echo "wrong"; } ?> . </h4></li>
+          <li><h4>They and I - Sie und Ich/sie und Ich. Your answer is <?php if ($_GET["ex1_4"] = "Sie und Ich" || $_GET["ex1_4"] = "sie und Ich") { echo "correct"; } else { echo "wrong"; } ?> . </h4></li>
+          <li><h4>You(formal) - Sie. Your answer is <?php if ($_GET["ex1_5"] = "Sie") { echo "correct"; } else { echo "wrong"; } ?> . </h4></li>
+        </ol>
+        <h2><a href="earthrock-r.github.io/german_2.html">Next Lesson</a></h2>
+      </div class="container">
+    </div>
+    <div id="footer">
+      <div class="container">
+      <div class="column-a">
+        <ul>
+          <li><a href="earthrock-r.github.io/index.html"><h4>HOME<h4></a></li>
+          <li><a href="earthrock-r.github.io/about.html"><h4>ABOUT US</h4></a></li>
+          <li><a href="earthrock-r.github.io/contact.html"><h4>CONTACT US</h4></a></li>
+        </ul>
+      </div>
+      <div class="qinfo">
+      </div>
+    </div class="container">
+    </div>
+  </body>
+</html>
